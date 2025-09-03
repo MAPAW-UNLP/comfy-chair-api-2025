aeb60bd6501dfde57dfc72e0d737b97aa0ae61b1
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2025-09-03 21:19:19 +0000

    agrega dummy endpoints

--- dummy/urls.py
+++ dummy/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .api import DummyAPI
+from .api import ListDummiesAPI, CreateDummyAPI, DummyAPI
 
 urlpatterns = [
+    path('', ListDummiesAPI.as_view(), name='list-dummies'),
+    path('new/', CreateDummyAPI.as_view(), name='create-dummy'),
     path('test/', DummyAPI.as_view(), name='test-endpoint'),
 ]
