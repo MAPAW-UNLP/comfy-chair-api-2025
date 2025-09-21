446e83f087ed3b47a48d2ec3988da8c7b5aa068d
Sofía Celano <sofia.celano181928@alumnos.info.unlp.edu.ar>
2025-09-20 21:27:47 -0300

    agrego los models de chairs en el admin

--- chairs/admin.py
+++ chairs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from chairs.models import Bidding, AsignacionRevisor, Revision
+
+# Registramos los modelos para que aparezca en el admin
+admin.site.register(Bidding)
+admin.site.register(AsignacionRevisor)
+admin.site.register(Revision)   
