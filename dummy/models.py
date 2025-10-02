df5a4178c86f0d3320cbac077ce5ae0b88acb658
Dolores Pondarré <dolopondarre@icloud.com>
2025-10-01 23:43:48 -0300

    Modifico nombres en modelos y atributos para que esten en ingles

--- dummy/models.py
+++ dummy/models.py
@@ -6,19 +6,19 @@ class Dummy(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Articulo(models.Model):
-    nombre = models.CharField(max_length=100)
+class Article(models.Model):
+    name = models.CharField(max_length=100)
     
 class Review(models.Model):
-    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
-    puntaje = models.IntegerField(
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    score = models.IntegerField(
         validators=[MinValueValidator(-3), MaxValueValidator(3)]
     )
     opinion = models.TextField()
     
 class Bid(models.Model):
-    ESTADOS = ["Interesado", "No Interesado", "Quizás"]
+    STATES = ["Interesado", "No Interesado", "Quizás"]
     
-    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
-    
-    estado =models.CharField(max_length=20, choices=ESTADOS, null=True, blank=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    choice = models.CharField(max_length=20, choices=STATES, null=True, blank=True)
