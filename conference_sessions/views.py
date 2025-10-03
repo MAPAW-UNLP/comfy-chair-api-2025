200fd103a69088e0ea4b95830c431e83695dd548
imartinezpass <imartinezpass@gmail.com>
2025-10-03 18:36:20 -0300

    Add filtering by conference_id in SessionViewSet

--- conference_sessions/views.py
+++ conference_sessions/views.py
@@ -5,3 +5,11 @@ from conference_sessions.serializers import SessionSerializer
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
+
+    # Optional: Filtra las sesiones por ID de conferencia
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        conference_id = self.request.query_params.get('conference_id')
+        if conference_id:
+            queryset = queryset.filter(conference_id=conference_id)
+        return queryset
