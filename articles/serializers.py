e47390037e2162690b915455749642209f56e085
imartinezpass <imartinezpass@gmail.com>
2025-10-03 11:52:02 -0300

    Refactor serializers to use nested ConferenceSerializer in Article and Session serializers

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from conference_sessions.models import Session
+from conferences.models import Conference
 from users.models import User
 from .models import Article
 
+class ConferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conference
+        fields = ['id', 'name' ]
+
+class SessionSerializer(serializers.ModelSerializer):
+    conference = ConferenceSerializer(read_only=True)
+
+    class Meta:
+        model = Session
+        fields = ['id', 'title', 'deadline', 'conference']
 
 class ArticleSerializer(serializers.ModelSerializer):
     authors = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
     notification_author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    session = serializers.PrimaryKeyRelatedField(queryset=Session.objects.all(), allow_null=True, required=False)
+    session = SessionSerializer(read_only=True)
 
     class Meta:
         model = Article
--- conference_sessions/serializers.py
+++ conference_sessions/serializers.py
@@ -3,8 +3,13 @@ from .models import Session
 from conferences.serializers import ConferenceSerializer
 from conferences.models import Conference
 
+class ConferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conference
+        fields = ['id', 'name' ]
+
 class SessionSerializer(serializers.ModelSerializer):
-    conference = serializers.PrimaryKeyRelatedField(queryset=Conference.objects.all())
+    conference = ConferenceSerializer(read_only=True)
 
     class Meta:
         model = Session
