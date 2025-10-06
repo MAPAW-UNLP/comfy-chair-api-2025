2641f4eba79c45f98d7c68f1b4e2c7e6402dc504
danielaDel <danidelgado979@gmail.com>
2025-10-05 22:23:17 -0300

    Se agregaron otros tres endpoints:se agregaron modelos necesarios extra, serializers, urls..

--- reviewer/api.py
+++ reviewer/api.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from reviewer.models import Review, Article, Bid
-from dummy.serializers import ArticleSerializer, BidSerializer
+from reviewer.models import Review, Article, Bid, User
+from dummy.serializers import ArticleSerializer, BidSerializer, BidUpdateSerializer
+from reviewer.serializers import ReviewerDetailSerializer
 
 # GET /api/articles
 class ArticleListView(APIView):
@@ -34,3 +36,44 @@ class BiddingView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# PUT /api/bidding/{id}
+class BiddingUpdateView(APIView):
+    def put(self, request, id):
+        #Busca el bid con el id, si no lo encuentra retorna 404
+        bid = get_object_or_404(Bid, id=id)
+        serializer = BidUpdateSerializer(bid, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# GET /api/bidding?reviewerId=123
+class ReviewerBidsView(APIView):
+    def get(self, request):
+        reviewer_id = request.GET.get('reviewerId')
+        if not reviewer_id:
+            return Response({"error": "reviewerId parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
+        #Se omite la verificacion del id, al no tener el modelo de User
+        bids = Bid.objects.filter(reviewer_id=reviewer_id)
+        serializer = BidSerializer(bids, many=True)
+        return Response(serializer.data)
+
+       
+# GET /api/reviewers/{id}
+class ReviewerDetailView(APIView):
+    def get(self,request,id):
+        try:
+            # Buscar cualquier usuario por ID, ver si agregar verificacion de roles mas adelante..
+            reviewer = User.objects.get(id=id)
+            serializer = ReviewerDetailSerializer(reviewer)
+            return Response(serializer.data)
+        except User.DoesNotExist:
+            return Response(
+                {"error": f"Usuario con ID {id} no encontrado"}, 
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+ 
--- reviewer/models.py
+++ reviewer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
@@ -9,6 +11,32 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+#Eliminar despues:
+class AssignmentReview(models.Model):
+    reviewer = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='assignmentreviews'
+    )
+    article = models.ForeignKey('articles.Article', on_delete=models.CASCADE)
+    reviewed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('reviewer', 'article')
+
+class User(models.Model):
+    full_name = models.CharField(_("Nombre Completo"), max_length=255)
+    affiliation = models.CharField(_("Afiliación"), max_length=255, blank=True)
+    email = models.EmailField(_("Email"), unique=True)
+    password = models.CharField(_("Contraseña"), max_length=128)
+    
+    is_author = models.BooleanField(default=True)
+    is_reviewer = models.BooleanField(default=False)
+    is_chair = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return self.email
     
 class Review(models.Model):
     reviewer = models.ForeignKey('auth.User')
