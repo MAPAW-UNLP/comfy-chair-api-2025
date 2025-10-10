535b63f95840a234909db57b35179121da3fe237
Anita Ormello Barral <anitaormellob@gmail.com>
2025-10-09 22:57:25 -0300

    Agrego '/' que faltaban

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .api import ArticleDetailView, ArticleListView, BiddingUpdateView, BiddingView, ReviewerBidsView, ReviewerDetailView  
 urlpatterns = [
     path('articles/', ArticleListView.as_view()),
-    path('articles/<int:pk>', ArticleDetailView.as_view()),
-    path('bidding', BiddingView.as_view()),
+    path('articles/<int:pk>/', ArticleDetailView.as_view()),
+    path('bidding/', BiddingView.as_view()),
     path('bidding/<int:id>/', BiddingUpdateView.as_view(), name='bidding-update'),
     path('bids/', ReviewerBidsView.as_view(), name='reviewer-bids'),
     path('reviewers/<int:id>/', ReviewerDetailView.as_view(), name='reviewer-detail'),
