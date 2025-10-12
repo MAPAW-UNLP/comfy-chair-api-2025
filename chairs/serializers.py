30e9831d0efd7a7609c10d29ac421eced5732272
Sofía Celano <sofia.celano181928@alumnos.info.unlp.edu.ar>
2025-10-12 20:49:26 -0300

    refactor views a api y nombre de variables clases etc en ingles

--- chairs/serializers.py
+++ chairs/serializers.py
@@ -1,10 +1,8 @@
-
 from rest_framework import serializers
+from chairs.models import ReviewAssignment
 
-from chairs.models import AssignmentReview
 
-
-class AssignmentReviewSerializer(serializers.ModelSerializer):
+class ReviewAssignmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = AssignmentReview
+        model = ReviewAssignment
         fields = ['id', 'reviewer', 'article', 'reviewed']
