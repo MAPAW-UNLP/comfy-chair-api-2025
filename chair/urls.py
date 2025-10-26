8af40c4a2b0241cd9596f7ddb4ff4f6c6803ed3b
Sofía Celano <sofia.celano181928@alumnos.info.unlp.edu.ar>
2025-10-25 21:10:32 -0300

    ajuste nombre endpoint

--- chair/urls.py
+++ chair/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('chair/new/', CreateReviewAssignmentAPI.as_view(), name='create-review-assignment'),
     path('chair/<int:reviewer_id>/<int:article_id>/delete/', DeleteReviewAssignmentAPI.as_view(), name='delete-review-assignment'),
     path('chair/articles/<int:article_id>/available-reviewers/', AvailableReviewersAPI.as_view(), name='available-reviewers'),
-    path("selection/fixed/<int:session_id>/", CutoffSelectionAPI.as_view(), name="selection-fixed"),
+    path("selection/cut-off/<int:session_id>/", CutoffSelectionAPI.as_view(), name="selection-cut-off"),
 ]
