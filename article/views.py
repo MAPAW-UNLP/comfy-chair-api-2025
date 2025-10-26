1d953cdfd2f893a9bc815207bf3ddaec543cac9d
AgustinAlejandroHeredia <102188628+AgustinAlejandroHeredia@users.noreply.github.com>
2025-10-26 19:04:37 -0300

    Agrego endpoint para article

--- article/views.py
+++ article/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from article.models import Article
 from .serializers import ArticleSerializer
 
+from rest_framework.decorators import action
+
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
 
@@ -20,3 +22,10 @@ class ArticleViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    # endpoint hecho por grupo 3 para obtener articulos en base a la id de una sesion dada
+    @action (detail=False, methods=['get'])
+    def getArticlesBySessionId (self, request, session_id):
+        articles = Article.objects.filter(session_id = session_id)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
