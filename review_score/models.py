79430c48c66e6c85c522b9dd4b65a5bc53e0bdf7
Sofía Celano <sofia.celano181928@alumnos.info.unlp.edu.ar>
2025-10-28 15:50:12 -0300

    fix score

--- review_score/models.py
+++ review_score/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from user.models import User
 from article.models import Article
 
 class ReviewScore(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review_scores')
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='review_scores')
-    score = models.DecimalField(max_digits=3, decimal_places=1)
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(-3),
+            MaxValueValidator(3)
+        ]
+    )
 
     class Meta:
         unique_together = ('user', 'article')
