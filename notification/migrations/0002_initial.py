c1eb1b96ac8bfbe5360a0e563c523e3c60018def
Dolores Pondarré <dolopondarre@icloud.com>
2025-11-28 12:26:30 -0300

    Corro de cero las migraciones por modificaciones en los modelos

--- notification/migrations/0002_initial.py
+++ notification/migrations/0002_initial.py
@@ -10,18 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('review_score', '0001_initial'),
+        ('notification', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='reviewscore',
+            model_name='notification',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='review_scores', to=settings.AUTH_USER_MODEL),
-        ),
-        migrations.AlterUniqueTogether(
-            name='reviewscore',
-            unique_together={('user', 'article')},
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='notifications', to=settings.AUTH_USER_MODEL),
         ),
     ]
