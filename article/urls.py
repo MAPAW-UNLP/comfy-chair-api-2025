4d13eccd8c8c347aeed3a1bc9b6c77e2bc34113d
Sofia Rodriguez <sofiarodrigueztec@gmail.com>
2025-12-31 14:20:02 -0300

    Soluciono conflictos merge

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet, ArticleDeletionRequestViewSet
+from .views import ArticleViewSet #, ArticleDeletionRequestViewSet No esta definido
 
 router = DefaultRouter()
 
 router.register(r'article', ArticleViewSet, basename='article')
-router.register(r'article-deletion-request', ArticleDeletionRequestViewSet, basename='article-deletion-request')
+# router.register(r'article-deletion-request', ArticleDeletionRequestViewSet, basename='article-deletion-request')
 
 urlpatterns = router.urls
